a00e9313b3f9d975f9b3660181192a9e12bcd46f
Alexander Choi <dev.alxndr@gmail.com>
2020-11-11 23:03:20 +0900

    web resource handler

---
+++ demo-springboot/src/main/java/io/alxndr/demospringboot/WebConfig.java
@@ -0,0 +1,16 @@
+package io.alxndr.demospringboot;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+public class WebConfig implements WebMvcConfigurer {
+
+    @Override
+    public void addResourceHandlers(ResourceHandlerRegistry registry) {
+        registry.addResourceHandler("/alxndr/**")
+                .addResourceLocations("classpath:/alxndr/")
+                .setCachePeriod(20);
+    }
+}
