59363856e6328b2677ee7d457369716e5e590188
seungwon <alxndr@grovesoft.net>
2020-11-13 13:00:32 +0900

    demo spring security

---
+++ demo-spring-security/src/main/java/io/alxndr/demospringsecurity/DemoSpringSecurityApplication.java
@@ -0,0 +1,13 @@
+package io.alxndr.demospringsecurity;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DemoSpringSecurityApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DemoSpringSecurityApplication.class, args);
+    }
+
+}
---
+++ demo-springsecurity/src/test/java/io/alxndr/demospringsecurity/IndexControllerTest.java
@@ -0,0 +1,9 @@
+import static org.junit.jupiter.api.Assertions.*;
+
+/**
+ * @author Alexander
+ * @date 2020-11-13
+ **/
+class IndexControllerTest {
+
+}
