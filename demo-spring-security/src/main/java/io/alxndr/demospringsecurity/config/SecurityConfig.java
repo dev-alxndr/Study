f35128ea022cba60b9e55a0c2ce042d8e60e3f55
Alexander Choi <dev.alxndr@gmail.com>
2020-11-14 22:00:21 +0900

    security setting

--- demo-spring-security/src/main/java/io/alxndr/demospringsecurity/config/SecurityConfig.java
+++ demo-spring-security/src/main/java/io/alxndr/demospringsecurity/config/SecurityConfig.java
@@ -1,8 +1,12 @@
 package io.alxndr.demospringsecurity.config;
 
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.crypto.factory.PasswordEncoderFactories;
+import org.springframework.security.crypto.password.NoOpPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
 
 @Configuration
 public class SecurityConfig extends WebSecurityConfigurerAdapter {
@@ -17,4 +21,11 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
                 .and()
             .httpBasic();   // without accept text/html
     }
+
+    @Bean
+    public PasswordEncoder passwordEncoder() {
+        return PasswordEncoderFactories.createDelegatingPasswordEncoder();
+//        return NoOpPasswordEncoder.getInstance();   // 절대 쓰지말걸.
+    }
+
 }
