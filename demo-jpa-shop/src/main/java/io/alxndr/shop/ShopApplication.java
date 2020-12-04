3d0ee21220412c3f14100eef4b911d1639ad9a8e
Alexander Choi <alxndr@grovesoft.net>
2020-12-04 10:15:01 +0900

    commit

--- demo-jpa-shop/src/main/java/io/alxndr/shop/ShopApplication.java
+++ demo-jpa-shop/src/main/java/io/alxndr/shop/ShopApplication.java
@@ -11,7 +11,7 @@ import java.util.List;
 public class ShopApplication {
 
     public static void main(String[] args) {
-        EntityManagerFactory emf = Persistence.createEntityManagerFactory("shopProperties");
+        EntityManagerFactory emf = Persistence.createEntityManagerFactory("shop");
 
         EntityManager em = emf.createEntityManager();
 
@@ -24,14 +24,7 @@ public class ShopApplication {
             newMember.setId(1L);
             newMember.setName("ALXNDR");
 
-//            em.persist(newMember);
-            List<Member> result = em.createQuery("select m from Member m", Member.class)
-                    .getResultList();
-
-            for (Member member : result) {
-                System.out.println("NAME = " + member.getName());
-            }
-
+            em.persist(newMember);
             tx.commit();
         } catch (Exception e) {
             tx.rollback();
