e8612caf0ef72d7051161c0c4b0d5082e0b2482a
Alexander Choi <alxndr@grovesoft.net>
2020-12-24 09:15:37 +0900

    member save & find

--- jpashop/src/main/java/io/alxndr/jpashop/MemberRepository.java
+++ jpashop/src/main/java/io/alxndr/jpashop/MemberRepository.java
@@ -10,6 +10,14 @@ public class MemberRepository {
     @PersistenceContext
     private EntityManager em;
 
+    public Long save(Member member) {
+        em.persist(member);
+        return member.getId();
+    }
+
+    public Member find(Long id) {
+        return em.find(Member.class, id);
+    }
 
 
 }
