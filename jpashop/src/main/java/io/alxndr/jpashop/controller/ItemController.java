fdd2afb483ab880021d45a24c4c83ace70a06f40
Alexander Choi <alxndr@grovesoft.net>
2020-12-31 12:50:09 +0900

    상품 리스트

--- jpashop/src/main/java/io/alxndr/jpashop/controller/ItemController.java
+++ jpashop/src/main/java/io/alxndr/jpashop/controller/ItemController.java
@@ -1,6 +1,7 @@
 package io.alxndr.jpashop.controller;
 
 import io.alxndr.jpashop.domain.item.Book;
+import io.alxndr.jpashop.domain.item.Item;
 import io.alxndr.jpashop.service.ItemService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Controller;
@@ -8,11 +9,21 @@ import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 
+import java.util.List;
+
 @Controller
 @RequiredArgsConstructor
 public class ItemController {
 
     private final ItemService itemService;
+
+    @GetMapping("/items")
+    public String list(Model model) {
+        List<Item> items = itemService.findItems();
+        model.addAttribute("items", items);
+
+        return "items/itemList";
+    }
 
     @GetMapping("/items/new")
     public String createForm(Model model) {
