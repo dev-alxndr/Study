ce5729997490de896617e3487f0c7c4bb76f599a
Alexander Choi <alxndr@grovesoft.net>
2021-01-05 09:35:55 +0900

    member 조회

--- spring-jpa/src/main/java/io/alxndr/jpashop/api/MemberApiController.java
+++ spring-jpa/src/main/java/io/alxndr/jpashop/api/MemberApiController.java
@@ -10,12 +10,51 @@ import org.springframework.web.bind.annotation.*;
 
 import javax.validation.Valid;
 import javax.validation.constraints.NotEmpty;
+import java.util.List;
+import java.util.stream.Collectors;
 
 @RestController     // @Controller + ResponseBody
 @RequiredArgsConstructor
 public class MemberApiController {
 
     private final MemberService memberService;
+
+    /*
+    * Entity를 직접 반환하면 안됨.
+    * 1. 엔티티에 프레젠테이션 계층을 위한 로직이 추가된다.
+    * 2. 엔티티에 모든 값이 노출된다.
+    * 3. 엔티티를 수정하면 API스펙이 바뀐다.
+    * */
+    @GetMapping("/api/v1/members")
+    public List<Member> membersV1() {
+        return memberService.findMembers();
+    }
+
+
+    @GetMapping("/api/v2/members")
+    public Result membersV2() {
+        List<Member> findMember = memberService.findMembers();
+
+        List<MemberDto> collect = findMember.stream()
+                .map(m -> new MemberDto(m.getName()))
+                .collect(Collectors.toList());
+
+        return new Result(collect);
+    }
+
+    @Data
+    @AllArgsConstructor
+    static class Result<T> {
+        private T data;
+    }
+
+    
+    @Data
+    @AllArgsConstructor
+    static class MemberDto {
+        private String name;
+    }
+
 
     @PostMapping("/api/v1/members")
     public CreateMemberResponse saveMemberV1(@RequestBody @Valid Member member) {
