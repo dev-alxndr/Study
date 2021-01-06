24166ce42015275c6903d14fa2090f0018cd2c2f
Alexander Choi <alxndr@grovesoft.net>
2021-01-06 09:08:57 +0900

    hibernate5module Lazy Force Loading 끄고 For로 강제 로딩

--- spring-jpa/src/main/java/io/alxndr/jpashop/api/OrderSimpleApiController.java
+++ spring-jpa/src/main/java/io/alxndr/jpashop/api/OrderSimpleApiController.java
@@ -36,6 +36,12 @@ public class OrderSimpleApiController {
     @GetMapping("/api/v1/simple-orders")
     public List<Order> ordersV1() {
         List<Order> all = orderRepository.findAllByCriteria(new OrderSearch());
+
+        for (Order order : all) {
+            order.getMember().getName();    // Lazy 강제 초기화
+            order.getDelivery().getStatus();
+        }
+
         return all;
     }
 
