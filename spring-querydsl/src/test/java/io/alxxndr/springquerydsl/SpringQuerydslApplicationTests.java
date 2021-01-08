cad69d429443a7e712a4b9651d64b3615b362bef
Alexander Choi <alxndr@grovesoft.net>
2021-01-08 14:51:38 +0900

    querydsl project

---
+++ spring-querydsl/src/test/java/io/alxxndr/springquerydsl/SpringQuerydslApplicationTests.java
@@ -0,0 +1,13 @@
+package io.alxxndr.springquerydsl;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringQuerydslApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
