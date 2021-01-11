254f0229a3dcd73979021ba1fe87fdb84e5489a4
Alexander Choi <alxndr@grovesoft.net>
2021-01-11 18:11:44 +0900

    commit

--- spring-querydsl/src/test/java/io/alxndr/springquerydsl/QuerydslBasicTest.java
+++ spring-querydsl/src/test/java/io/alxndr/springquerydsl/QuerydslBasicTest.java
@@ -469,4 +469,33 @@ public class QuerydslBasicTest {
         }
     }
 
+    // 프로젝션 대상이 하나
+    @Test
+    public void simpleProjection() throws Exception {
+        List<String> result = queryFactory
+                .select(member.username)
+                .from(member)
+                .fetch();
+
+        for (String s : result) {
+            System.out.println("s = " + s);
+        }
+    }
+
+
+    @Test
+    public void tupleProjection()throws Exception {
+
+        List<Tuple> result = queryFactory
+                .select(member.username, member.age)
+                .from(member)
+                .fetch();
+        for (Tuple tuple : result) {
+            String username = tuple.get(member.username);
+            Integer age = tuple.get(member.age);
+            System.out.println("username = " + username);
+            System.out.println("age = " + age);
+        }
+    }
+
 }
