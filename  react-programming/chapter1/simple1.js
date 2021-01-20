af0babf5bfebaf52c2b26b56b843249846c8ac8b
Alexander Choi <alxndr@grovesoft.net>
2021-01-20 13:15:05 +0900

    react

---  react-programming/chapter1/simple1.js
+++  react-programming/chapter1/simple1.js
@@ -1,38 +1,29 @@
 function LikeButton() {
-    const [liked, setLiked] = React.useState(false);
-    const text = liked ? "좋아요 취소" : "좋아요";
-    return React.createElement(
-        'button',
-        {onClick: () => setLiked(!liked)},
-        text
-    );
+  const [liked, setLiked] = React.useState(false);
+  const text = liked ? "좋아요 취소" : "좋아요";
+  return /*#__PURE__*/React.createElement("button", {
+    onClick: () => setLiked(!liked)
+  }, text);
 }
 
 function Container() {
-    const [count, setCount] = React.useState(0);
-    return React.createElement(
-        'div',
-        null,
-        React.createElement(LikeButton),
-        React.createElement(
-            'div',
-            null,
-            React.createElement(
-                'div',
-                { style: { marginTop: 20 } },
-                React.createElement('span', null, '현재 카운트: '),
-                React.createElement('span', { style: { marginRight: 10 } }, count),
-                React.createElement(
-                    'button',
-                    { onClick: () => setCount(count + 1) },
-                    "증가"
-                ),
-                React.createElement(
-                    'button',
-                    { onClick: () => setCount(count - 1) },
-                    "감소"
-                )
-            )
-        )
-    )
+  const [count, setCount] = React.useState(0);
+  return /*#__PURE__*/React.createElement("div", null, /*#__PURE__*/React.createElement(LikeButton, null), /*#__PURE__*/React.createElement("div", {
+    style: {
+      marginTop: 20
+    }
+  }, /*#__PURE__*/React.createElement("span", null, " \uD604\uC7AC \uCE74\uC6B4\uD2B8 : "), /*#__PURE__*/React.createElement("span", {
+    style: {
+      marginTop: 10,
+      color: 'red'
+    }
+  }, " ", count, " "), /*#__PURE__*/React.createElement("button", {
+    onClick: () => setCount(count + 1)
+  }, "\uC99D\uAC00"), /*#__PURE__*/React.createElement("button", {
+    onClick: () => setCount(count - 1)
+  }, "\uAC10\uC18C")));
 }
+
+;
+const domContainer = document.getElementById("root");
+ReactDOM.render(React.createElement(Container), domContainer);
