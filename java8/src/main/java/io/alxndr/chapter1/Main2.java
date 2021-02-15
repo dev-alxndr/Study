8a9ce44527d09dadd20568fec551f83bbb70e548
Alexander Choi <dev.alxndr@gmail.com>
2021-02-15 16:02:06 +0900

    기본 Function

--- java8/src/main/java/io/alxndr/chapter1/Main2.java
+++ java8/src/main/java/io/alxndr/chapter1/Main2.java
@@ -1,6 +1,7 @@
 package io.alxndr.chapter1;
 
-import java.util.function.Function;
+import java.util.Enumeration;
+import java.util.function.*;
 
 public class Main2 {
     public static void main(String[] args) {
@@ -39,6 +40,42 @@ public class Main2 {
         * */
         Integer apply4 = plus10.andThen(multiply2).apply(2);
         System.out.println("apply4 = " + apply4);
+
+        /*
+        Consumer
+        * T 타입을 받아서 아무값도 리턴하지 않는 함수 인터페이스
+        * void Accept(T t)
+        * */
+        Consumer<Integer> printT = (i) -> System.out.println(i);
+        printT.accept(10);
+
+        /*
+        *   Supplier
+        *   T 타입의 값을 제공하는 함수 인터페이스
+        *   T get()
+        * */
+        Supplier<Integer> get10 = () -> 10;
+        System.out.println("get10. = " + get10.get());
+
+        /*
+        * T 타입을 받아서 boolean을 리턴하는 함수 인터페이스
+        * - boolean test(T t)
+        *
+        * 함수 조합용 메소드
+        *   - And
+        *   - Or
+        *   - Negate (not)
+        * */
+        Predicate<String> startsWithA = (s) -> s.startsWith("A");
+        System.out.println("startsWithA.test(\"Alxndr\") = " + startsWithA.test("Alxndr"));
+        Predicate<Integer> isEven = (num) -> num % 2 == 0;
+        System.out.println("isEven.test(\"20\") = " + isEven.test(20));
+
+        /*
+        * UnaryOperator 입력값과 결과값의 타입이 같은 경우
+        * */
+        UnaryOperator<Integer> plus100 = (i) -> i + 100;
+        System.out.println("plus100.apply(1) = " + plus100.apply(1));
 
 
     }
