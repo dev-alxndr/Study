f3eb4f8763a1a57b153004f527ac3c3c4cc0ec88
Alexander Choi <dev.alxndr@gmail.com>
2021-03-02 18:31:20 +0900

    security project

---
+++ inflearn-spring-security/src/main/java/io/alxndr/inflearnspringsecurity/InflearnSpringSecurityApplication.java
@@ -0,0 +1,13 @@
+package io.alxndr.inflearnspringsecurity;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class InflearnSpringSecurityApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(InflearnSpringSecurityApplication.class, args);
+    }
+
+}
---
+++ inflearn-spring-security/src/test/java/io/alxndr/inflearnspringsecurity/InflearnSpringSecurityApplicationTests.java
@@ -0,0 +1,13 @@
+package io.alxndr.inflearnspringsecurity;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class InflearnSpringSecurityApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
